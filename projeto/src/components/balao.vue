9ce77a61225b47d85f7ae1ccc6ae3b0d0188cd0e
Marcelo <marcelo.souza@dce.ufpb.br>
2019-08-22 12:35:42 -0300

    add componetes

---
+++ projeto/src/components/balao.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="balao">
+    <fala></fala>
+    <button class="btn btn-primary" type="submit">Button</button>
+  </div>
+</template>
+
+<script>
+import fala from "./fala.vue";
+export default {
+  name: "Balao",
+  components: {
+    fala
+  }
+};
+</script>
+
+
+<style>
+#balao {
+  width: 400px;
+  height: 300px;
+  border: 1px solid black;
+  border-radius: 100%;
+  padding: 5%;
+}
+</style>
---
+++ projeto/src/components/fala.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>{{ falas.fala1 }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      falas: {
+        boasVindas: "Olá, O meu nome é Enzo!",
+        fala1:
+          "Tenho 9 anos, nasci na cidade de Rio Tinto, na Universidade Federal da Paraíba - Campus IV.",
+        fala2:
+          "Eu gosto muito das cores, porque elas conseguem transmitir sentimentos.",
+        fala3: "Agora eu quero conhecer mais sobre você.",
+        aviso:
+          "Agora, iremos tentar expressar o que sentimos com as cores que iremos ver."
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+    h1 {
+        font-size: 15px;
+        text-align: justify;
+    }
+</style>
